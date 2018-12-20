11a8e29490fa853c9f84b47874bb849b20ec2436
Anggit Prayogo <anggitprayogo@gmail.com>
2018-12-20 21:11:25 +0700

    initial commit, membuat navigation pada nav bottom

---
+++ app/src/main/java/com/anggitprayogo/forcastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.anggitprayogo.forcastmvvm.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment: Fragment() {
+
+}
---
+++ app/src/main/java/com/anggitprayogo/forcastmvvm/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.anggitprayogo.forcastmvvm.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel;
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
